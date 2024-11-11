bc73442b22056c81dee9fddc9c80694ac6d8d6d2
chepizhkoandrew <andrii.chepizhko@gmail.com>
2024-11-11 13:00:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000; // Use the PORT environment variable or default to 3000
 
 app.use(express.json());
 
