cd177664962d27753e305eebc80c748400e88cda
chepizhkoandrew <andrii.chepizhko@gmail.com>
2024-11-12 22:29:41 +0100

    acvwvr

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import OpenAI from "openai";
 import dotenv from 'dotenv';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
