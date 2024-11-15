945bd8be745b7c9532e21c2bf8a37bf94679d877
chepizhkoandrew <andrii.chepizhko@gmail.com>
2024-11-15 01:16:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
-const PORT = process.env.PORT || 10000;
 
 
+const PORT = 443;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
